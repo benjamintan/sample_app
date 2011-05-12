ce627b157354dad583039008d59588f3ac4fca2e
Benjamin Tan <rambo@ramb0x.(none)>
2011-05-12 14:12:43 +0800

    Added sign up form

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -82,10 +82,6 @@ describe User do
       User.new(@attr.merge(:password => "", :password_confirmation => "")).should_not be_valid
     end
     
-    it "should require a matching password confirmation"  # do
-    #  User.new(@attr.merge(:password_confirmation))
-    #end    
-    
     it "should reject short passwords" do
       short = "a" * 5
       hash = @attr.merge(:password => short, :password_confirmation => short)
